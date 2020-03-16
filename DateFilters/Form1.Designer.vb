1997f4d7ca75b167ca5764793e5efe19bb3f6aa2
Marc <marcalfaro@live.com>
2020-03-16 22:04:46 +0800

    Add project files.

---
+++ DateFilters/Form1.Designer.vb
@@ -0,0 +1,165 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.cboDate = New System.Windows.Forms.ComboBox()
+        Me.btnCancel = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.btnSelect = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.dtpFrom = New System.Windows.Forms.DateTimePicker()
+        Me.dtpTo = New System.Windows.Forms.DateTimePicker()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.cboDate)
+        Me.GroupBox1.Controls.Add(Me.btnCancel)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Controls.Add(Me.btnSelect)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.Label5)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.dtpFrom)
+        Me.GroupBox1.Controls.Add(Me.dtpTo)
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 12)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(316, 202)
+        Me.GroupBox1.TabIndex = 37
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Choose the date range below:"
+        '
+        'cboDate
+        '
+        Me.cboDate.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.cboDate.FormattingEnabled = True
+        Me.cboDate.Items.AddRange(New Object() {"Today", "Date Range", "As on Date", "Current Month", "Last Month", "Current Week", "Last Week", "Current Year", "Last Year"})
+        Me.cboDate.Location = New System.Drawing.Point(102, 31)
+        Me.cboDate.Name = "cboDate"
+        Me.cboDate.Size = New System.Drawing.Size(182, 21)
+        Me.cboDate.TabIndex = 1
+        '
+        'btnCancel
+        '
+        Me.btnCancel.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.btnCancel.Location = New System.Drawing.Point(179, 158)
+        Me.btnCancel.Name = "btnCancel"
+        Me.btnCancel.Size = New System.Drawing.Size(75, 28)
+        Me.btnCancel.TabIndex = 4
+        Me.btnCancel.Text = "Cancel"
+        Me.btnCancel.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(32, 34)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(58, 13)
+        Me.Label1.TabIndex = 28
+        Me.Label1.Text = "Date Filter:"
+        '
+        'btnSelect
+        '
+        Me.btnSelect.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btnSelect.Location = New System.Drawing.Point(59, 158)
+        Me.btnSelect.Name = "btnSelect"
+        Me.btnSelect.Size = New System.Drawing.Size(75, 28)
+        Me.btnSelect.TabIndex = 0
+        Me.btnSelect.Text = "Select"
+        Me.btnSelect.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(32, 73)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(58, 13)
+        Me.Label2.TabIndex = 26
+        Me.Label2.Text = "Start Date:"
+        '
+        'Label5
+        '
+        Me.Label5.BackColor = System.Drawing.Color.Gray
+        Me.Label5.Location = New System.Drawing.Point(14, 146)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(286, 1)
+        Me.Label5.TabIndex = 33
+        Me.Label5.Text = " "
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(32, 106)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(55, 13)
+        Me.Label3.TabIndex = 25
+        Me.Label3.Text = "End Date:"
+        '
+        'dtpFrom
+        '
+        Me.dtpFrom.CustomFormat = "ddd dd MMM yyyy h:mm tt"
+        Me.dtpFrom.Format = System.Windows.Forms.DateTimePickerFormat.Custom
+        Me.dtpFrom.Location = New System.Drawing.Point(102, 67)
+        Me.dtpFrom.Name = "dtpFrom"
+        Me.dtpFrom.Size = New System.Drawing.Size(182, 20)
+        Me.dtpFrom.TabIndex = 2
+        '
+        'dtpTo
+        '
+        Me.dtpTo.CustomFormat = "ddd dd MMM yyyy h:mm tt"
+        Me.dtpTo.Format = System.Windows.Forms.DateTimePickerFormat.Custom
+        Me.dtpTo.Location = New System.Drawing.Point(102, 100)
+        Me.dtpTo.Name = "dtpTo"
+        Me.dtpTo.Size = New System.Drawing.Size(182, 20)
+        Me.dtpTo.TabIndex = 3
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(346, 228)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents cboDate As ComboBox
+    Friend WithEvents btnCancel As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents btnSelect As Button
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents dtpFrom As DateTimePicker
+    Friend WithEvents dtpTo As DateTimePicker
+End Class
---
+++ DateFilters/Form1.vb
@@ -0,0 +1,61 @@
+﻿Public Class Form1
+    Private Sub btnSelect_Click(sender As Object, e As EventArgs) Handles btnSelect.Click
+        DialogResult = Windows.Forms.DialogResult.OK
+    End Sub
+
+    Private Sub btnCancel_Click(sender As Object, e As EventArgs) Handles btnCancel.Click
+        DialogResult = Windows.Forms.DialogResult.Cancel
+    End Sub
+
+    Private Sub f_FilterDate_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        cboDate.SelectedIndex = 0
+    End Sub
+
+    Private Sub cboDate_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cboDate.SelectedIndexChanged
+
+        'dtpFrom.Enabled = IIf(cboDate.SelectedIndex = 1, True, False)
+        'dtpTo.Enabled = IIf(cboDate.SelectedIndex = 1, True, False)
+
+        Select Case cboDate.SelectedIndex
+            Case 0, 1  'Today / Date Range
+                dtpFrom.Value = New Date(Now.Year, Now.Month, Now.Day, 0, 0, 0)
+                dtpTo.Value = Now
+
+            Case 2  'As on Date
+                dtpFrom.Value = New Date(1900, 1, 1)
+                dtpTo.Value = Now
+
+            Case 3  'Current Month
+                dtpFrom.Value = New Date(Now.Year, Now.Month, 1, 0, 0, 0)
+                dtpTo.Value = Now
+
+            Case 4  'Last Month
+                dtpFrom.Value = New Date(Now.Year, Now.Month - 1, 1, 0, 0, 0)
+                dtpTo.Value = New Date(Now.Year, Now.Month - 1, System.DateTime.DaysInMonth(Now.Year, Now.Month - 1), 23, 59, 59)
+
+            Case 5  'Current Week
+
+                Dim fdw As DateTime = DateTime.Today.AddDays(-Weekday(DateTime.Today, FirstDayOfWeek.System) + 1)
+                Dim ldw As DateTime = DateTime.Today.AddDays(-Weekday(DateTime.Today, FirstDayOfWeek.System) + 7)
+
+                dtpFrom.Value = fdw
+                dtpTo.Value = New Date(ldw.Year, ldw.Month, ldw.Day, 23, 59, 59)
+
+            Case 6  'Last Week
+
+                Dim fdw As DateTime = DateTime.Today.AddDays(-Weekday(DateTime.Today, FirstDayOfWeek.System) + 1).AddDays(-7)
+                Dim ldw As DateTime = DateTime.Today.AddDays(-Weekday(DateTime.Today, FirstDayOfWeek.System) + 7).AddDays(-7)
+
+                dtpFrom.Value = fdw
+                dtpTo.Value = New Date(ldw.Year, ldw.Month, ldw.Day, 23, 59, 59)
+
+            Case 7  'Current Year
+                dtpFrom.Value = New Date(Now.Year, 1, 1, 0, 0, 0)
+                dtpTo.Value = Now
+
+            Case 8  'Last Year
+                dtpFrom.Value = New Date(Now.Year - 1, 1, 1, 0, 0, 0)
+                dtpTo.Value = New Date(Now.Year, 1, 1, 23, 59, 59).AddDays(-1)
+        End Select
+    End Sub
+End Class
